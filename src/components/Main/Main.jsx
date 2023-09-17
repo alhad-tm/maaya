862687a6eed04b12b92a01107fef21d4e82df7ee
alhad-tm <alhadchikkutm@gmail.com>
2023-09-17 10:54:17 +0530

    added animation

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -13,6 +13,15 @@ const Main = () => {
                     <div className={css.box}>
                         <img src={MaayaS} alt="" />
                     </div>
+                    <div className={css.animation}>
+     <span>  Jewellery for every occasion </span> 
+     <span>Return Gifts</span>
+     <span> Party wear bangles</span>
+     <span>Bridal packages</span>
+     <span>Bulk orders accepted</span>
+     <span>View Store</span>
+
+                    </div>
                     <button className={css.vsbtn}>View store</button>
                 </div>
 
@@ -24,6 +33,24 @@ const Main = () => {
             <div className={css.contain}>
                     <div className={css.box}>
                         <img src={MaayaR} alt="" />
+                    </div>
+                    <div className={css.animation2}>
+                        <div className={css.aleft}>
+                            <span>Kundan stones(All types)</span>
+                            <span>Bangles</span>
+                            <span>Thread,Glue</span>
+                            <span>Sugar</span>
+                            <span>Tube beads</span>
+                            <span>Hair clips</span>
+                        </div>
+                        <div className={css.aright}>
+                            <span>Packing plastic box</span>
+                            <span>Partition boxes</span>
+                            <span>Wrapped bangles</span>
+                            <span>Stone chain</span>
+                            <span>Pearl chain</span>
+                            <span>Saree pin</span>
+                        </div>
                     </div>
                     <button className={css.vsbtn}>View store</button>
                 </div>
